e0fb59850a07f0744f2be7c282d03cd6c69b98c1
shin <sinej1020@gmail.com>
2024-06-20 22:24:16 +0900

    feat: suspense

--- components/layouts/MainLayout.tsx
+++ components/layouts/MainLayout.tsx
@@ -1,19 +1,23 @@
 'use client'
 
-import React from 'react';
+import React, {Suspense} from 'react';
 import TopMenu from "@/components/layouts/includes/TopMenu";
 import MainHeader from "@/components/layouts/includes/MainHeader";
 import SubMenu from "@/components/layouts/includes/SubMenu";
 import Footer from "@/components/layouts/includes/Footer";
+import Loading from "@/app/(site)/loading";
 
 const MainLayout = ({ children }: { children: React.ReactNode }) => {
     return (
         <div id="main_layout">
+            <Suspense fallback={<Loading />}>
+
             <TopMenu />
             <MainHeader />
             <SubMenu />
             {children}
             <Footer />
+            </Suspense>
         </div>
     );
 };
--- components/ui/cart/CartItem.tsx
+++ components/ui/cart/CartItem.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const CartItem = () => {
     return (
-        <div>
+        <div className="disabled:">
 
         </div>
     );
